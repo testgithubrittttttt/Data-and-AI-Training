d83de88fb384079355adb7faa1fa017ad3e473bb
Dhruv Sharma <134517753+testgithubrittttttt@users.noreply.github.com>
2025-09-22 12:09:52 +0530

    Create evenodd.py

---
+++ Sep 21/Daily Activities/evenodd.py
@@ -0,0 +1,10 @@
+# def evenOdd(num):
+#     if num % 2 == 0:
+#         return f"{num} is even"
+#     else:
+#         return f"{num} is odd"
+#
+# number = int(input("enter your number for checking: - "))
+# answer = evenOdd(number)
+# print(answer)
+

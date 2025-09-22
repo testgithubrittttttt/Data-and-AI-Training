f3817284bcbb16a47974eaa42e276e7d4cd26df9
Dhruv Sharma <134517753+testgithubrittttttt@users.noreply.github.com>
2025-09-22 11:04:41 +0530

    Update evenOdd.py

--- Daily Activity/evenOdd.py
+++ Daily Activity/evenOdd.py
@@ -1,1 +1,11 @@
+# def evenOdd(num):
+#     if num % 2 == 0:
+#         return f"{num} is even"
+#     else:
+#         return f"{num} is odd"
+#
+# number = int(input("enter your number for checking: - "))
+# answer = evenOdd(number)
+# print(answer)
+
 

62d8f4786ff4e6275c51868d5af38e59ba27061c
Dhruv Sharma <134517753+testgithubrittttttt@users.noreply.github.com>
2025-09-22 15:40:31 +0530

    Update PandasCode.py

--- September 22/Daily Activities/PandasCode.py
+++ September 22/Daily Activities/PandasCode.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 data = {
@@ -8,12 +9,16 @@ data = {
 
 df = pd.DataFrame(data)
 print(df)
-print(df["name"])#one column accessing
-print(df[["name","age"]])#multiple column accessing
-print(df.iloc[0])#for row vise fetching the data
-print(df.loc[2, "age"])#for column vise fetching the data
+# print(df["name"])#one column accessing
+# print(df[["name","age"]])#multiple column accessing
+# print(df.iloc[0])#for row vise fetching the data
+# print(df.loc[2, "age"])#for column vise fetching the data
+#
+# #filtering data
+# old_age = df[df["age"] > 30]
+# print(old_age)
 
-#filtering data
-old_age = df[df["age"] > 30]
-print(old_age)
-
+#adding and updating the columns
+df["Result"] = np.where(df["age"] > 30,"old","young")
+df.loc[df["name"] == "john" , "course"] == "ccvt"
+print(df["Result"])

1523b41180882fe5ad899f6eea1b376f04bbe6f0
Dhruv Sharma <134517753+testgithubrittttttt@users.noreply.github.com>
2025-09-22 14:39:56 +0530

    Create NumpyCode.py

---
+++ September 22/Daily Activities/NumpyCode.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr_1 = np.array([10,20,30])
+arr_2 = np.array([[10,20,30],[20,50,60]])
+print(arr_1)
+print(arr_2)
+
+print(arr_1.max())
+print(arr_1.min())
+print(arr_1.mean())
+print(arr_1.std())
+print(arr_1[:3])
+print(arr_1[::-1])

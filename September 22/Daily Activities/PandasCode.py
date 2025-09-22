88e1708fca198777b87689f0202953d50520fa05
Dhruv Sharma <134517753+testgithubrittttttt@users.noreply.github.com>
2025-09-22 15:29:24 +0530

    Create PandasCode.py

---
+++ September 22/Daily Activities/PandasCode.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data = {
+    "name": ["john","johnny","dilip"],
+    "age" : [22,23,55],
+    "course" : ["ai","ml","dl"]
+}
+
+df = pd.DataFrame(data)
+print(df)
+print(df["name"])#one column accessing
+print(df[["name","age"]])#multiple column accessing
+print(df.iloc[0])#for row vise fetching the data
+print(df.loc[2, "age"])#for column vise fetching the data
+
+#filtering data
+old_age = df[df["age"] > 30]
+print(old_age)
+

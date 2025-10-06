5c665d43d9570d3dc26f64802bed0c17fe6481df
Dhruv Sharma <134517753+testgithubrittttttt@users.noreply.github.com>
2025-10-06 11:03:19 +0530

    Create yaml_creator_firstcode.py

---
+++ 6 October/Daily activities/yaml_creator_firstcode.py
@@ -0,0 +1,16 @@
+import yaml
+
+#python directory
+config = {
+    "model":"RandomForest",
+    "params":{
+        "n_estimators":100,
+        "max_depth":5
+    },
+    "dataset": "students.csv"
+}
+with open("config.yaml","w") as f:
+    yaml.dump(config,f)
+with open("config.yaml","r") as f:
+    data = yaml.safe_load(f)
+print(data["params"]["n_estimators"])

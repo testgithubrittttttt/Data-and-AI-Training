5106ba34529231fce521d23e61a93da54c023673
Dhruv Sharma <134517753+testgithubrittttttt@users.noreply.github.com>
2025-10-20 23:26:49 +0530

    Enhance module4_producer.py with explanatory comments
    Added comments to clarify the code functionality and purpose.

--- 17th october/tasks/MILESTONE PROJECT/module4_producer.py
+++ 17th october/tasks/MILESTONE PROJECT/module4_producer.py
@@ -1,12 +1,12 @@
-import pika
-import json
+import pika #Python client library for RabbitMQ.
+import json #to convert Python dictionaries (orders) into JSON strings (so they can be sent as messages).
 
 # Connect to RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost')) #Connects to RabbitMQ running locally (localhost).
+channel = connection.channel() #its like creating communication line where you send messages.
 
 # Declare queue
-channel.queue_declare(queue='order_queue', durable=True)
+channel.queue_declare(queue='order_queue', durable=True) #connects to) a queue named 'order_queue' and ensuring the queue persists even if RabbitMQ restarts thats why we do durable -true.
 
 # Example new orders
 new_orders = [
@@ -15,14 +15,16 @@ new_orders = [
 ]
 
 for order in new_orders:
-    message = json.dumps(order)
-    channel.basic_publish(
-        exchange='',
-        routing_key='order_queue',
+    message = json.dumps(order) #Converts each order dictionary into a JSON string as it is a universal format.
+    
+    channel.basic_publish( #make this for sending the message to RabbitMQ.
+        exchange='', #using the default exchange
+        routing_key='order_queue',#specifies which queue to send the message to
         body=message,
         properties=pika.BasicProperties(
-            delivery_mode=2,  # make message persistent
+            delivery_mode=2,  # make message persistent helps in saving data to disk, not lost if RabbitMQ restarts
         ))
     print(f"Sent order {order['OrderID']}")
 
 connection.close()
+

94f3a05efb3cb969bee9e529adcd8f2d119d2490
Dhruv Sharma <134517753+testgithubrittttttt@users.noreply.github.com>
2025-10-20 16:49:12 +0530

    Update module1.py

--- 17th october/tasks/MILESTONE PROJECT/module1.py
+++ 17th october/tasks/MILESTONE PROJECT/module1.py
@@ -1,13 +1,13 @@
-import pandas as pd
-import sqlite3
+import pandas as pd #library used for handling CSV files and dataframes easily.
+import sqlite3 #built-in Python module that allows you to create and interact with SQL and databases
 
 # Connect to SQLite DB (or create it)
-conn = sqlite3.connect("retail.db")
-cursor = conn.cursor()
+conn = sqlite3.connect("retail.db") #Creating a file named retail.db that acts as the database.
+cursor = conn.cursor() #with the help of cursor we can now be able to use and execute the sql commands in the databases
 
 # Create tables
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS products ( #this line is good to right for ensuring that this table will not be created again if it is excisting already
     ProductID TEXT PRIMARY KEY,
     ProductName TEXT,
     Category TEXT,
@@ -29,14 +29,14 @@ products_df = pd.read_csv("products.csv")
 customers_df = pd.read_csv("customers.csv")
 
 # Insert data
-products_df.to_sql("products", conn, if_exists="replace", index=False)
-customers_df.to_sql("customers", conn, if_exists="replace", index=False)
+products_df.to_sql("products", conn, if_exists="replace", index=False) #if_exists = is table exists then replace it with completely new data
+customers_df.to_sql("customers", conn, if_exists="replace", index=False) #index=False prevents Pandas from adding an extra index column.
 
-conn.commit()
+conn.commit() #saves) all changes made so far to the database file.
 
 # Add a new product
-cursor.execute("INSERT INTO products VALUES (?, ?, ?, ?)",
-               ("P105", "Webcam", "Accessories", 70))
+cursor.execute("INSERT INTO products VALUES (?, ?, ?, ?)", #using placeholder instaedof directly writing because placeholders are safer(prevents sql injection) and are reusable(same query for different products)
+               ("P105", "Webcam", "Accessories", 70)) #placeholder when using it takes input as a tuple
 conn.commit()
 
 # Update product price
@@ -46,7 +46,7 @@ conn.commit()
 
 # Delete a customer
 cursor.execute("DELETE FROM customers WHERE CustomerID = ?",
-               ("C002",))
+               ("C002",))# we use comma(,) because its not string its a tuple
 conn.commit()
 
 # List all customers from India
@@ -55,4 +55,5 @@ cursor.execute("SELECT * FROM customers WHERE Country = ?",
 print("Customers from India:")
 for row in cursor.fetchall():
     print(row)
+
 
